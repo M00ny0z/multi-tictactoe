f727728c0ecd062dcc96bc4ee8ef89ed0cb2cc60
Manny Munoz <em66@uw.edu>
2021-02-03 13:48:59 -0800

    Now opens global websocket connection

--- main.js
+++ main.js
@@ -2,17 +2,31 @@ var gameEngine = new GameEngine();
 const EMPTY = 0;
 const CIRCLE = 1;
 const CROSS = 2;
+let gameMap;
 
 var ASSET_MANAGER = new AssetManager();
 
+const socket = new WebSocket('ws://localhost:3000');
 
 ASSET_MANAGER.downloadAll(function () {
 	var canvas = document.getElementById('gameWorld');
 	var ctx = canvas.getContext('2d');
 
+	gameMap = new Map(gameEngine);
+
 	gameEngine.init(ctx);
 
-	gameEngine.addEntity(new Map(gameEngine));
+	gameEngine.addEntity(gameMap);
 
 	gameEngine.start();
+
+	socket.addEventListener('open', function (event) {
+		const gameState = gameMap.getState();
+		socket.send(JSON.stringify(gameState));
+	});
+});
+
+// Listen for messages
+socket.addEventListener('message', function (event) {
+	console.log(`GameState: ${event.data}`);
 });
